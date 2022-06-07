74f553b1c4f6cbc360e2210fb1e2c9d5c2d629b9
gabriel <gabriel_aq7@hotmail.com>
2022-06-07 09:45:40 -0300

    upd app, coluna

--- src/app/components/coluna/coluna.component.ts
+++ src/app/components/coluna/coluna.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-coluna",
@@ -6,7 +6,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./coluna.component.css"],
 })
 export class ColunaComponent implements OnInit {
-  titulo: string = "";
+  @Input() titulo: string = "";
   constructor() {}
 
   ngOnInit(): void {}
