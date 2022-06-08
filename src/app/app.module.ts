05a5431b059d48d2f327ec83a71bbb721b0eda0b
gabriel <gabriel_aq7@hotmail.com>
2022-06-08 17:55:58 -0300

    add card; upd coluna

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,17 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { ColunaComponent } from "./components/coluna/coluna.component";
+import { CardComponent } from "./components/card/card.component";
+import { DragDropModule } from "@angular/cdk/drag-drop";
 
 @NgModule({
-  declarations: [AppComponent, ColunaComponent],
+  declarations: [AppComponent, ColunaComponent, CardComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatCardModule,
+    DragDropModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
