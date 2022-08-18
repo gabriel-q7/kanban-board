34540360c7fc453b91afb9a1c07e66b05eed0489
gabriel <gabriel_aq7@hotmail.com>
2022-08-18 17:19:46 -0300

    upds

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, Routes } from "@angular/router";
+import { MainViewComponent } from "./pages/main-view/main-view.component";
+
+const routes: Routes = [{ path: "", component: MainViewComponent }];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,19 @@ import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
-import { ColunaComponent } from "./components/coluna/coluna.component";
-import { CardComponent } from "./components/card/card.component";
 import { DragDropModule } from "@angular/cdk/drag-drop";
+import { MainViewComponent } from "./pages/main-view/main-view.component";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
-  declarations: [AppComponent, ColunaComponent, CardComponent],
+  declarations: [AppComponent, MainViewComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatCardModule,
     DragDropModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
